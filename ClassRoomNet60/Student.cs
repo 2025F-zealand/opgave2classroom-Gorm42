264e577fd09758bbb013eb1cab2f7dbfa2f70c48
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-02-06 13:14:16 +0100

    Oprettet Student, properties for Name, BirthDay, BirthMonth & ctor med tre parametre.

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+
+
+        public string Name { get { return Name; } }
+        public int BirthMonth { get { return BirthMonth; } }
+        public int BirthDay { get { return BirthDay; } }
+
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            name = Name;
+            birthMonth = BirthMonth;
+            birthDay = BirthDay;
+
+        }
+
+
+    }
+}
